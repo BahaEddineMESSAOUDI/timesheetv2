894f5e02c242e296e642d46d776fb23b8d4e7968
BahaEddineMESSAOUDI <bahaeddine.messaoudi@esprit.tn>
2021-12-04 11:34:45 +0100

    NOK

--- src/test/java/tn/esprit/spring/services/TimesheetServiceTest.java
+++ src/test/java/tn/esprit/spring/services/TimesheetServiceTest.java
@@ -1,8 +1,6 @@
 package tn.esprit.spring.services;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest 
