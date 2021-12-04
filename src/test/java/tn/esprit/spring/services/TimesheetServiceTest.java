dbd77f2c5198450dccf369ba2bcd91ce294bf9a9
BahaEddineMESSAOUDI <bahaeddine.messaoudi@esprit.tn>
2021-12-04 11:35:20 +0100

    JUNITFIXED

--- src/test/java/tn/esprit/spring/services/TimesheetServiceTest.java
+++ src/test/java/tn/esprit/spring/services/TimesheetServiceTest.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.services;
 
-
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest 
