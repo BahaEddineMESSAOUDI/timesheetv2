2a1a0f9769bd1485a51d341e135a38b99d8b17fe
BahaEddineMESSAOUDI <bahaeddine.messaoudi@esprit.tn>
2021-12-04 11:17:00 +0100

    TesthouldbeOK

--- src/test/java/tn/esprit/spring/services/TimesheetServiceTest.java
+++ src/test/java/tn/esprit/spring/services/TimesheetServiceTest.java
@@ -1,4 +1,4 @@
-package com.esprit.spring.service;
+package tn.esprit.spring.services;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,5 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest 
 class TimesheetServiceTest {
-
+	
 }
